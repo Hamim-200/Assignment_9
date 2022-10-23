fdea6156c7c3877277c3f0ba2f9af4f877f4ecd7
Hamim Ahmed <hamim.ahmed484@gmail.com>
2022-10-23 19:58:28 +0600

    blog css added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import './Blogs.css'
 
 const Blogs = () => {
   return (
-    <div>
-
+    <div className='blog-container'>
       <div>
-        <h2>What is the purpose of React router?</h2>
-        <p>Ans:ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
+        <h3>What is the purpose of React router?</h3>
+        <p>Ans: ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
       </div>
       <div>
-        <h2>How does context api work?</h2>
-        <p>Ans:The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+        <h3>How does context api work?</h3>
+        <p>Ans: The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
       </div>
       <div>
-        <h2>Write Details about useref hook?</h2>
-        <p>Ans:The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.</p>
+        <h3>Write Details about useref hook?</h3>
+        <p>Ans: The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.</p>
       </div>
       {/* <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
         <div className="collapse-title text-xl font-medium">
